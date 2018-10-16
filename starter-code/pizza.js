1991f4f9e4e4a98935057b0c77ce484286c8ca7d
“Eullin <eullinch@gmail.com>
2018-10-16 18:31:17 +0200

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,42 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+    
+  });
+
+$(".crust").removeClass("crust-gluten-free")
+
+$(".sauce").removeClass("sauce-white")
+
+$(".btn-pepperonni").click(function(){
+    $(".pep").toggle(".hidden");
+    $(".btn.btn-pepperonni").toggleClass("active")
+});
+
+$(".btn-mushrooms").click(function(){
+    $(".mushroom").toggle(".hidden");
+    $(".btn.btn-mushrooms").toggleClass("active")
+});
+
+$(".btn-green-peppers").click(function(){
+    $(".green-pepper").toggle(".hidden");
+    $(".btn.green-peppers").toggleClass("active")
+});
+
+$(".btn-sauce").click(function(){
+    $(".sauce").toggle("sauce-white")
+})
+
+/* $(".btn-pepperonni").click(function(){
+    $(".btn btn-pepperonni active").css(".btn btn-pepperonni btn:active")
+}) */
+
+
+/* gluten free
+$(".btn-green-peppers").click(function(){
+    $(".green-pepper").toggle(".hidden");
+}); */
+
+
+
+
+
